d7a68d0ca513ec356925f35339162e477ca92002
Alvaro Quintana <alvaroqp@gmail.com>
2019-12-24 12:57:10 +0100

    delete FLAG_ACTIVITY_NEW_TASK on intent

--- app/src/main/java/es/architectcoders/mascotas/ui/routers/GenericRouter.kt
+++ app/src/main/java/es/architectcoders/mascotas/ui/routers/GenericRouter.kt
@@ -8,11 +8,9 @@ import es.architectcoders.mascotas.ui.activities.ProfileActivity
 
 fun AppCompatActivity.goToEditProfileActivity(){
     val intent = Intent(this, EditProfileActivity::class.java)
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     this.startActivity(intent)
 }
 fun AppCompatActivity.goToProfileActivity(){
     val intent = Intent(this, ProfileActivity::class.java)
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     this.startActivity(intent)
 }
