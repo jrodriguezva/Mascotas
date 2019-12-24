b6749b88e6f175a96a5764cfa4777d4096d661f0
Alvaro Quintana <alvaroqp@gmail.com>
2019-12-24 13:05:21 +0100

    delete class unused

--- app/src/main/java/es/architectcoders/mascotas/ui/model/EditProfileRepository.kt
+++
@@ -1,8 +0,0 @@
-package es.architectcoders.mascotas.ui.model
-
-class EditProfileRepository() {
-
-    suspend fun updateUser(name: String, surname: String, address: String, postalCode: String, email: String) {
-
-    }
-}
