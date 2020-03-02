efb5860ebf3964f3f755a4c3872ba80b1799b27d
jmriballo <josemriballo@gmail.com>
2020-03-02 19:06:14 +0100

    Binding adapter view visible

--- app/src/main/java/es/architectcoders/mascotas/ui/common/Extensions.kt
+++ app/src/main/java/es/architectcoders/mascotas/ui/common/Extensions.kt
@@ -1,5 +1,7 @@
 package es.architectcoders.mascotas.ui.common
 
+import android.view.View
+import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -8,3 +10,13 @@ fun <T, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T) -> Unit) 
     liveData.observe(this, Observer(body))
 }
 
+@BindingAdapter("visible")
+fun View.setVisible(visible: Boolean?) {
+    visibility =
+        if(visible == true) {
+            View.VISIBLE
+        }
+        else {
+            View.GONE
+        }
+}
