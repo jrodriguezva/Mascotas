f29179d80432d863cdb4ebf36756cf920ff49abf
Juan Rodríguez <jrodriguezva@gmail.com>
2020-03-18 16:23:46 +0100

    get data from firebase

--- data/src/main/java/es/architectcoders/data/repository/AdvertRepository.kt
+++ data/src/main/java/es/architectcoders/data/repository/AdvertRepository.kt
@@ -3,17 +3,8 @@ package es.architectcoders.data.repository
 import es.architectcoders.data.datasource.FirestoreDataSource
 import es.architectcoders.domain.Advert
 
-
 class AdvertRepository(private val firestoreDataSource: FirestoreDataSource) {
-    suspend fun findRelevantAdverts(): List<Advert> {
-        return findDummyAdverts()
-    }
-
-    private fun findDummyAdverts() = listOf(
-        Advert("0", "Advert title 1", "https://picsum.photos/id/112/256", "10.5", true),
-        Advert("1", "Advert title 2", "https://picsum.photos/id/228/256", "25.000", true),
-        Advert("2", "Advert title 3", "https://picsum.photos/id/1080/256", "19.999", false)
-    )
+    suspend fun findRelevantAdverts(): List<Advert> = firestoreDataSource.getAdverts()
 
     suspend fun createAdvert(advert: Advert) = firestoreDataSource.addAdvert(advert)
 }
